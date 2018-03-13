9e1b10ca823e413fe828d60f8662ae81c315ad51
dbzahariev <dbzahariev@gmail.com>
2018-03-13 11:22:37 +0200

    Add new files

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace Algorithms
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("dotnet");
         }
     }
 }
