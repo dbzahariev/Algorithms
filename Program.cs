5150c5768d380a57e4c7aec0ef2a402fd03e486c
dbzahariev <dbzahariev@gmail.com>
2018-03-20 19:09:45 +0200

    Add new files

--- Program.cs
+++ Program.cs
@@ -8,19 +8,7 @@ namespace Algorithms
         static void Main(string[] args)
         {
             Console.WriteLine("Hi");
-            int[] numbers = Console.ReadLine()
-                .Split(separator: ' ')    
-                .Select(int.Parse)
-                .ToArray();
-            
-            Console.WriteLine(numbers);
             Console.WriteLine("END");
-            Console.WriteLine(Sum(numbers));
-        }
-
-        private static bool Sum(int[] numbers)
-        {
-            throw new NotImplementedException();
         }
     }
 }
