8a6327507b2f460dea47b459ebcc740f9446b9b9
Unknown <dbzahariev@gmail.com>
2018-03-22 21:52:07 +0200

    Add new files

---
+++ L01/P01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace P01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(SumArray(arr));
+        }
+
+        private static int SumArray(int[] arr, int index = 0)
+        {
+            if (index == arr.Length - 1)
+            {
+                return arr[index];
+            }
+
+            return arr[index] + SumArray(arr, index + 1);
+}
+    }
+}
