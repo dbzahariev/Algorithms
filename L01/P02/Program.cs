6d1cfc694c9b65e97ddfbf1b55a8fe2ed4479a3c
Unknown <dbzahariev@gmail.com>
2018-03-22 22:08:31 +0200

    Add new files

---
+++ L01/P02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace P02
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine(GetFactorial(num));
+        }
+
+        private static int GetFactorial(int num)
+        {
+            if (num < 0)
+            {
+                throw new ArgumentOutOfRangeException("Number must be non-negative!");
+            }
+
+            if (num < 2)
+            {
+                return 1;
+            }
+
+            return num * GetFactorial(num - 1);
+        }
+    }
+}
---
+++ L01/P03_Recursive_Drawing/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace P03_Recursive_Drawing
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int size = int.Parse(Console.ReadLine());
+            Draw(size);
+        }
+
+        private static void Draw(int size)
+        {
+            if (size < 1)
+            {
+                return;
+            }
+
+            Console.WriteLine(new string('*', size));
+            Draw(size - 1);
+            Console.WriteLine(new string('#', size));
+        }
+    }
+}
