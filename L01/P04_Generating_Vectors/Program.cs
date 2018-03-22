e6f02832e73f339ef1904a2367e31c93da9e7f88
Unknown <dbzahariev@gmail.com>
2018-03-22 22:18:33 +0200

    Add new files

---
+++ L01/P04_Generating_Vectors/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P04_Generating_Vectors
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int vectorLength = int.Parse(Console.ReadLine());
+            GenerateVectors(vectorLength, new int[vectorLength]);
+        }
+
+        private static void GenerateVectors(int vectorLength, int[] vector, int currIndex = 0)
+        {
+            if (currIndex == vectorLength)
+            {
+                Console.WriteLine(string.Join("", vector));
+            }
+            else
+            {
+                vector[currIndex] = 0;
+                GenerateVectors(vectorLength, vector, currIndex + 1);
+
+                vector[currIndex] = 1;
+                GenerateVectors(vectorLength, vector, currIndex + 1);
+            }
+        }
+    }
+}
---
+++ L01/P05_Generating_Combinations/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace P05_Generating_Combinations
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int k = int.Parse(Console.ReadLine());
+            GenerateCombinations(numbers, new int[k]);
+        }
+
+        private static void GenerateCombinations(int[] numbers, int[] combinations, int currIndex = 0, int border = 0)
+        {
+            if (combinations.Length == currIndex)
+            {
+                Console.WriteLine(string.Join(" ", combinations));
+            }
+            else
+            {
+                for (int i = border; i < numbers.Length; i++)
+                {
+                    combinations[currIndex] = numbers[i];
+                    GenerateCombinations(numbers, combinations, currIndex + 1, i + 1);
+                }
+            }
+        }
+    }
+}
